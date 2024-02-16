02bcdef6ee45d1224621ff9523726f91d3d4ce53
Edward Ibarra <edwardibarra@Edwards-MacBook-Air.local>
2024-02-15 23:34:23 -0500

    ignore file added

--- fea/target/debug/deps/fea-23a65eac921c930c.d
+++
@@ -1,5 +0,0 @@
-/Users/edwardibarra/Source-Code/FEA/fea/target/debug/deps/fea-23a65eac921c930c: src/main.rs
-
-/Users/edwardibarra/Source-Code/FEA/fea/target/debug/deps/fea-23a65eac921c930c.d: src/main.rs
-
-src/main.rs:
